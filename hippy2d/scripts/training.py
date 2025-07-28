8b593575ab7ba4a99d9d300162275b5b843771ba
Tomas Karella <karella@lanl.gov>
2025-07-28 15:39:36 -0600

    Fixing lightning

--- hippy2d/scripts/training.py
+++ hippy2d/scripts/training.py
@@ -10,14 +10,18 @@ from src.factory import get_datamodule, get_model
 from src.models import InvNet
 from src.utils import ClickDictionaryType
 
+# Work arround for automatic SLURM detection
+from lightning.pytorch.plugins.environments import SLURMEnvironment
+SLURMEnvironment.detect = lambda: False
+
 @click.command()
 @click.option('-n', '--run_name', default='default', help='Name of the experiment.')
 @click.option('--early_stopping', default=3, type=int, help='Early stopping patience.')
 @click.option('--epochs', default=10, type=int, help='Number of training epochs.')
 @click.option('--debug', is_flag=True, default=False, help='Run in debug mode with small datasets.')
 @click.option('--dataset_name', default='MnistRotTest', type=str, help='Name of the dataset.')
-@click.option('--d_hparams', default=dict(batch_size=32, data_dir='data', pad=0, to_complex=True), type=ClickDictionaryType(), help='Dataset hyperparameters.')
-@click.option('--model_name', default='ResHNeXtv3', type=str, help='Name of the model to use.')
+@click.option('--d_hparams', default=dict(batch_size=32, data_dir='data', pad=0, to_complex=False), type=ClickDictionaryType(), help='Dataset hyperparameters.')
+@click.option('--model_name', default='Resnet18', type=str, help='Name of the model to use.')
 @click.option('--m_param', default=dict(), type=ClickDictionaryType(), help='Model hyperparameters.')
 @click.option('--optimizer_name', default='AdamW', type=str, help='Optimizer name.')
 @click.option('--optimizer_hparams', default=dict(lr=1e-3), type=ClickDictionaryType(), help='Optimizer hyperparameters.')
