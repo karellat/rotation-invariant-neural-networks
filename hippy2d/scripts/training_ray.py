2886fd4002862ae2e8b714264ab8a5bc73dd5f3a
Tomas Karella <karella@lanl.gov>
2025-08-08 15:45:15 -0600

    Rolling back

--- hippy2d/scripts/training_ray.py
+++ hippy2d/scripts/training_ray.py
@@ -1,115 +1,5 @@
-import os
-import torch
-import tempfile
-import pytorch_lightning as pl
-import torch.nn.functional as F
-from filelock import FileLock
-from torchmetrics import Accuracy
-from torch.utils.data import DataLoader, random_split
-from torchvision.datasets import MNIST
-from torchvision import transforms
-
-
-class MNISTClassifier(pl.LightningModule):
-    def __init__(self, config):
-        super(MNISTClassifier, self).__init__()
-        self.accuracy = Accuracy(task="multiclass", num_classes=10, top_k=1)
-        self.layer_1_size = config["layer_1_size"]
-        self.layer_2_size = config["layer_2_size"]
-        self.lr = config["lr"]
-
-        # mnist images are (1, 28, 28) (channels, width, height)
-        self.layer_1 = torch.nn.Linear(28 * 28, self.layer_1_size)
-        self.layer_2 = torch.nn.Linear(self.layer_1_size, self.layer_2_size)
-        self.layer_3 = torch.nn.Linear(self.layer_2_size, 10)
-        self.eval_loss = []
-        self.eval_accuracy = []
-
-    def cross_entropy_loss(self, logits, labels):
-        return F.nll_loss(logits, labels)
-
-    def forward(self, x):
-        batch_size, channels, width, height = x.size()
-        x = x.view(batch_size, -1)
-
-        x = self.layer_1(x)
-        x = torch.relu(x)
-
-        x = self.layer_2(x)
-        x = torch.relu(x)
-
-        x = self.layer_3(x)
-        x = torch.log_softmax(x, dim=1)
-
-        return x
-
-    def training_step(self, train_batch, batch_idx):
-        x, y = train_batch
-        logits = self.forward(x)
-        loss = self.cross_entropy_loss(logits, y)
-        accuracy = self.accuracy(logits, y)
-
-        self.log("ptl/train_loss", loss)
-        self.log("ptl/train_accuracy", accuracy)
-        return loss
-
-    def validation_step(self, val_batch, batch_idx):
-        x, y = val_batch
-        logits = self.forward(x)
-        loss = self.cross_entropy_loss(logits, y)
-        accuracy = self.accuracy(logits, y)
-        self.eval_loss.append(loss)
-        self.eval_accuracy.append(accuracy)
-        return {"val_loss": loss, "val_accuracy": accuracy}
-
-    def on_validation_epoch_end(self):
-        avg_loss = torch.stack(self.eval_loss).mean()
-        avg_acc = torch.stack(self.eval_accuracy).mean()
-        self.log("ptl/val_loss", avg_loss, sync_dist=True)
-        self.log("ptl/val_accuracy", avg_acc, sync_dist=True)
-        self.eval_loss.clear()
-        self.eval_accuracy.clear()
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        return optimizer
-
-
-class MNISTDataModule(pl.LightningDataModule):
-    def __init__(self, batch_size=128):
-        super().__init__()
-        self.data_dir = tempfile.mkdtemp()
-        self.batch_size = batch_size
-        self.transform = transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
-        )
-
-    def setup(self, stage=None):
-        with FileLock(f"{self.data_dir}.lock"):
-            mnist = MNIST(
-                self.data_dir, train=True, download=True, transform=self.transform
-            )
-            self.mnist_train, self.mnist_val = random_split(mnist, [55000, 5000])
-
-            self.mnist_test = MNIST(
-                self.data_dir, train=False, download=True, transform=self.transform
-            )
-
-    def train_dataloader(self):
-        return DataLoader(self.mnist_train, batch_size=self.batch_size, num_workers=4)
-
-    def val_dataloader(self):
-        return DataLoader(self.mnist_val, batch_size=self.batch_size, num_workers=4)
-
-    def test_dataloader(self):
-        return DataLoader(self.mnist_test, batch_size=self.batch_size, num_workers=4)
-
-
-default_config = {
-    "layer_1_size": 128,
-    "layer_2_size": 256,
-    "lr": 1e-3,
-}
+from ray import tune
+from ray.tune.schedulers import ASHAScheduler
 from ray.train.lightning import (
     RayDDPStrategy,
     RayLightningEnvironment,
@@ -117,31 +7,70 @@ from ray.train.lightning import (
     prepare_trainer,
 )
 
+from hippy2d.trainer import get_trainer
 
-def train_func(config):
-    dm = MNISTDataModule(batch_size=config["batch_size"])
-    model = MNISTClassifier(config)
+def _config_test(config):
+    assert "seed" in config, "Training function requires 'seed' in config."
+    assert "epochs" in config, "Training function requires 'epochs' in config."
+    assert "dataset_name" in config, "Training function requires 'dataset_name' in config."
+    assert "dataset_hparams" in config, "Training function requires 'dataset_hparams' in config."
+    assert "model_name" in config, "Training function requires 'model_name' in config."
+    assert "m_param" in config, "Training function requires 'm_param' in config."
+    assert "optimizer_name" in config, "Training function requires 'optimizer_name' in config."
+    assert "optimizer_hparams" in config, "Training function requires 'optimizer_hparams' in config."
+    assert "lr_name" in config, "Training function requires 'lr_name' in config."
+    assert "lr_hparams" in config, "Training function requires 'lr_hparams' in config."
 
-    trainer = pl.Trainer(
-        devices="auto",
-        accelerator="auto",
-        strategy=RayDDPStrategy(),
-        callbacks=[RayTrainReportCallback()],
-        plugins=[RayLightningEnvironment()],
-        enable_progress_bar=False,
-    )
+def train_func(config): 
+    # Test the keys 
+    _config_test(config)
+    trainer, model, dm = get_trainer(seed=config["seed"],
+                          epochs=config["epochs"],
+                          dataset_name=config["dataset_name"],
+                          d_hparams=config["dataset_hparams"],
+                          model_name=config["model_name"],
+                          m_param=config["m_param"],
+                          optimizer_name=config["optimizer_name"],
+                          optimizer_hparams=config["optimizer_hparams"],
+                          lr_name=config["lr_name"],
+                          lr_hparams=config["lr_hparams"],
+                          accelerator="auto",
+                          trainer_callbacks=[RayTrainReportCallback()],
+                          trainer_params=dict(strategy=RayDDPStrategy(),
+                                              plugins=[RayLightningEnvironment()],
+                                              enable_progress_bar=False,
+                          ))
     trainer = prepare_trainer(trainer)
-    trainer.fit(model, datamodule=dm)
-
-from ray import tune
-from ray.tune.schedulers import ASHAScheduler
+    trainer.fit(model, datamodule=dm)   
 
 search_space = {
-    "layer_1_size": tune.choice([32, 64, 128]),
-    "layer_2_size": tune.choice([64, 128, 256]),
-    "lr": tune.loguniform(1e-4, 1e-1),
-    "batch_size": tune.choice([32, 64]),
+    "seed": 42,
+    "epochs": 100,
+    "dataset_name": "MnistRotTest", 
+    "model_name" : "PrototypeOptimalInvCNN",
+    "m_param": {
+        "in_channels" : 1,
+        "input_size" : 64,
+        "num_classes" : 10,
+        "zero_order_scaling" : tune.choice([True, False]),
+    },
+    "optimizer_hparams": {
+            "lr": tune.loguniform(1e-4, 1e-1),
+    }, 
+    "optimizer_name": "AdamW",
+    "dataset_hparams": {
+        "batch_size": tune.choice([32, 64, 128]),
+        "data_dir" : "/vast/home/karella/rotation-invariant-neural-networks/hippy2d/data",
+        "pad" : 0,
+        "to_complex" : False
+    },
+    "lr_name": "MultiStepLR",
+    "lr_hparams": {
+        "milestones": [30, 80],
+        "gamma": 0.1
+    }
 }
+_config_test(search_space)
 
 # The maximum training epochs
 num_epochs = 5
@@ -149,23 +78,24 @@ num_epochs = 5
 # Number of samples from parameter space
 num_samples = 10
 
-scheduler = ASHAScheduler(max_t=num_epochs, grace_period=1, reduction_factor=2)
-
 from ray.train import RunConfig, ScalingConfig, CheckpointConfig
 
 scaling_config = ScalingConfig(
-    num_workers=2, use_gpu=True, resources_per_worker={"CPU": 1, "GPU": 1}
+        num_workers=2, 
+        use_gpu=True,
+        resources_per_worker={"CPU": 10, "GPU":1}
 )
 
 run_config = RunConfig(
     checkpoint_config=CheckpointConfig(
         num_to_keep=2,
-        checkpoint_score_attribute="ptl/val_accuracy",
+        checkpoint_score_attribute="val_acc",
         checkpoint_score_order="max",
     ),
 )
 
 from ray.train.torch import TorchTrainer
+
 
 # Define a TorchTrainer without hyper-parameters for Tuner
 ray_trainer = TorchTrainer(
@@ -174,21 +104,17 @@ ray_trainer = TorchTrainer(
     run_config=run_config,
 )
 
-def tune_mnist_asha(num_samples=10):
-    scheduler = ASHAScheduler(max_t=num_epochs, grace_period=1, reduction_factor=2)
+scheduler = ASHAScheduler(max_t=num_epochs, grace_period=1, reduction_factor=2)
 
-    tuner = tune.Tuner(
+tuner = tune.Tuner(
         ray_trainer,
         param_space={"train_loop_config": search_space},
         tune_config=tune.TuneConfig(
-            metric="ptl/val_accuracy",
+            metric="val_acc",
             mode="max",
             num_samples=num_samples,
             scheduler=scheduler,
         ),
-    )
-    return tuner.fit()
-
-
-results = tune_mnist_asha(num_samples=num_samples)
-results.get_best_result(metric="ptl/val_accuracy", mode="max")
+)
+results = tuner.fit()
+results.get_best_result(metric="val_acc", mode="max")
