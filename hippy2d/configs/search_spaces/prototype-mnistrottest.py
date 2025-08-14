4730eaa83960513073620169d1943b823ed359bf
Tomas Karella <karella@lanl.gov>
2025-08-14 14:15:30 -0600

    Fixing search_spaces

--- hippy2d/configs/search_spaces/prototype-mnistrottest.py
+++ hippy2d/configs/search_spaces/prototype-mnistrottest.py
@@ -22,7 +22,6 @@ search_space = {
         "batch_size": 128,
         "data_dir" : "/vast/home/karella/rotation-invariant-neural-networks/hippy2d/data",
         "to_complex" : False,
-        "augment" : True 
     },
     "lr_name": "MultiStepLR",
     "lr_hparams": {
